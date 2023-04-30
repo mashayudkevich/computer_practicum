75b136d2e8fdc7b3dc1edd7f5e462ec607f0dabe
mashayudkevich <104457965+mashayudkevich@users.noreply.github.com>
2023-04-30 16:52:09 +0300

    main.cpp

---
+++ knapsack/main.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(int capacity,char**value) {
+ char*file=value[1];
+ ifstream fin(file);
+ ofstream data;
+ data.open("data.dzn");
+ long long n,W,p0,w0,i,j;
+ fin>>n>>W;
+ data <<"n = " << n << ';' << endl;
+ data << "W = " << W << ';' << endl;
+ vector <long long> p(n);
+ vector <long long> w(n);
+ for (i=0;i<n;i++){
+  fin>>p0>>w0;
+  p[i]=p0;
+  w[i]=w0;}
+ data << "profit = [";
+ for (i=0;i<n;i++){
+  data << p[i] << ", ";}
+ data << "];";
+ data << endl;
+ data << "size = [";
+ for (i =0;i<n;i++){
+  data << w[i] << ", ";}
+ data << "];";
+ data << endl;
+ data.close();
+ fin.close();
+ system("minizinc --solver Gecode minizinc.mzn data.dzn");
+ return 0;}
