2dd7f60406d4f6e9077531bf2f5a8a493265ec6a
mashayudkevich <104457965+mashayudkevich@users.noreply.github.com>
2023-05-01 11:40:40 +0300

    main.cpp

--- knapsack/main.cpp
+++ knapsack/main.cpp
@@ -10,22 +10,21 @@ int main(int capacity,char**value) {
  data.open("data.dzn");
  long long n,W,p0,w0,i,j;
  fin>>n>>W;
- data <<"n = " << n << ';' << endl;
- data << "W = " << W << ';' << endl;
+ data <<"n = " <<n<< ';' <<endl;
+ data <<"W = " <<W<< ';' <<endl;
  vector <long long> p(n);
  vector <long long> w(n);
  for (i=0;i<n;i++){
   fin>>p0>>w0;
-  p[i]=p0;
-  w[i]=w0;}
+  p[i]=p0;w[i]=w0;}
  data << "profit = [";
  for (i=0;i<n;i++){
-  data << p[i] << ", ";}
+  data <<p[i]<< ", ";}
  data << "];";
  data << endl;
  data << "size = [";
  for (i =0;i<n;i++){
-  data << w[i] << ", ";}
+  data <<w[i]<< ", ";}
  data << "];";
  data << endl;
  data.close();
